cdb83595c57233e0e1132d4ceb1c5501c705d04e
vlado <forsing@gmail.com>
2015-07-04 16:59:42 +0200

    Tagging feature completed

--- app/controllers/tags_controller.rb
+++ app/controllers/tags_controller.rb
@@ -1,12 +1,33 @@
 class TagsController < ApplicationController
 
-		
+	def index
+  		@tags = Tag.all
+	end	
 
 	def show
 	  @tag = Tag.find(params[:id])
 	end
 
-		
+
+
+
+
+
+
+	def destroy
+		@tag = Tag.find(params[:id])
+		@tag.taggings.destroy_all
+		@tag.destroy
+
+		flash.notice = "Tag '#{@tag.name}' Destroyed!"
+
+		redirect_to ('/tags')
+	end
+
+
+
+
+
 
 
 end
